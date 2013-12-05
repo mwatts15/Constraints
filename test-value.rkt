bc18879400b0f2e209c0c350a155ccd2422cbbef
Mark Watts <markw@cs.utexas.edu>
2013-12-05 15:15:58 -0600

    Added tests for some of our values

---
+++ test-value.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require "value.rkt")
+(require rackunit)
+(require rackunit/text-ui)
+
+(define tests
+  (list
+    (test-suite "Range"
+                (test-case "Range addition"
+                           (let ([r1 (new Range (start 2) (end 3))]
+                                 [r2 (new Range (start 5) (end 7))])
+                             (let ([r3 (send r1 add r2)])
+                               (check-eq? 7 (get-field start r3))
+                               (check-eq? 10 (get-field end r3))))))
+    (test-suite "Union"
+                (test-case "Union subset"
+
+(for ([t tests])
+  (run-tests t 'verbose))
--- value.rkt
+++ value.rkt
@@ -10,10 +10,46 @@
       #t
       #f)
     (super-new)))
+
 (define ValueBox
   (class Box
     (init [value false])
     (if (is-a? value Box)
       (super-make-object value)
       (super-new))))
+
+(define Set
+  (interface is-subset? is-member?))
+    
+  ; define a set interface
+  ; define functions on sets
+  ;   union, intersection, etc.
+; A union of Sets
+(define Union
+  (class* (Set) object%
+          [init
+          (define my-sets '())
+    (define (is-subset? a-set)
+      (sequence-andmap (lambda (x) (ormap (cut is-member? <> x) my-sets))
+              (send a-set values)))))
+
+(define Range
+  (class* (Set) object%
+    (super-new)
+    (init-field start end)
+    (define/public (add other)
+      (cond [(number? other)
+             (add-number other)]
+            [(is-a? other Range)
+             (add-range other)]))
+
+    (define (add-number number)
+      (new Range [start (+ start number)] [end (+ end number)]))
+
+    (define (add-range range)
+      (new Range 
+           [start (+ (get-field start range) start)]
+           [end (+ (get-field end range) end)]))))
+(define Class
+  (class Set
 
