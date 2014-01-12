c6f4f38f759d11ff37c14719f7207e6c3e3cc92b
Mark Watts <markw@cs.utexas.edu>
2014-01-12 10:32:16 -0600

    Non-constraint stuff in math-lang.rkt. prefix math to cml converter

--- math-types.rkt
+++ math-types.rkt
@@ -84,45 +84,6 @@
             (unless (even? iv)
               (send i setValue! (* 2 iv) this))))))))
 
-(define (toInfix expr)
-  (match expr
-    [`(,op ,lhs ,rhs) `(,(toInfix lhs) ,op ,(toInfix rhs))]
-    [x x]))
-(define (mathOpt expr)
-  (match (match expr
-                [`(+ ,x ,x) `(* 2 ,x)]
-                [`(+ ,x (+ ,x ,y)) `(+ (* 2 ,x) ,y)]
-                [`(+ ,x (- ,x ,y)) `(- ,y)]
-                [`(- ,x (+ ,x ,y)) y]
-                [`(/ 1 (/ ,x ,y)) `(/ ,y ,x)]
-                [`(/ ,x 1) x]
-                [`(,(or '+ '-) ,x 0) x]
-                [`(* ,x 1) x]
-                [`(* ,x 0) 0]
-                [`(- ,x ,x) 0]
-                [`(+ (- ,x) ,y) `(- ,y ,x)]
-                [`(- ,x (- ,y)) `(+ ,x ,y)]
-                [`(= ,(and (not 0) x) ,y) `(= 0 (- ,y ,x))]
-                ;combining terms
-                [`(,(and (or '+ '-) op)
-                    (* ,c1 ,x)
-                    (* ,c2 ,x)) `(* (,op ,c1 ,c2) ,x)]
-                [`(,(and (or '+ '-) op)
-                    ,x
-                    (* ,c2 ,x)) `(* (,op 1 ,c2) ,x)]
-                [`(,(and (or '+ '-) op)
-                    (* ,c1 ,x)
-                    ,x) `(* (,op ,c1 1) ,x)]
-                ; heuristic: move numbers to the outside
-                [`(,(and (or '+ '-) op1)
-                    (,(and (or '+ '-) op2) ,x ,(? number? y))
-                    ,(? (negate number?) z))
-                  `(,op2 (,op1 ,x ,z) ,y)]
-                ; the 'infamous' flip rule. probably shouldn't be here...
-                [`(,(and (or '+ '*) op) ,x ,y) `(,op ,y ,x)]
-                [x x])
-         [`(,op ,x ...) (cons op (map mathOpt x))]
-         [x x]))
 
 (define Equal
   (class Constraint
